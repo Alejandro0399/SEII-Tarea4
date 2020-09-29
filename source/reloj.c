a26865107d3aded58ecb452b4a0577f90db97ff6
Ethandrake Castillo <ie714410@iteso.mx>
2020-09-29 14:11:54 -0600

    funciones de reloj añadidas

--- source/reloj.c
+++ source/reloj.c
@@ -1,6 +1,44 @@
 #include "reloj.h"
 
-void segundero()
-{
 
+void segundero(void)
+{
+  static uint8_t segundos = 0;
+  segundos++;
+  if(alarma_seg == segundos)
+  {
+    //generar una notificacion
+  }
+  if(LIMITE_SEGUNDOS == segundos)
+  {
+    segundos = 0;
+    minutero();
+  }
+}
+
+void minutero(void)
+{
+  static uint8_t minutos = 0;
+  if(alarma_min == minutos)
+  {
+    //generar una notificacion
+  }
+  if(LIMITE_MINUTOS == minutos)
+  {
+    minutos = 0;
+    horas();
+  }
+}
+
+void horas(void)
+{
+  static uint8_t hora = 0;
+  if(alarma_hr == hora)
+  {
+    //generar una notificacion
+  }
+  if(LIMITE_HORAS == hora)
+  {
+    hora = 0;
+  }
 }
--- source/reloj.h
+++ source/reloj.h
@@ -1,6 +1,13 @@
 #ifndef RELOJ_H_
 #define RELOJ_H_
+
 #include "fsl_pit.h"
 #include <stdio.h>
-#define LIMITE_SEGUNDOS
+#define LIMITE_SEGUNDOS 60U
+#define LIMITE_MINUTOS  60U
+void segundero(void);
+void minutero(void);
+void horas(void);
+
+
 #endif
